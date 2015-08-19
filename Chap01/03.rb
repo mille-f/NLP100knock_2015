de539e5a9ee18996fa2802d3e4d7a6b0cd2ca454
mille-f <f11235tetra@gmail.com>
2015-08-20 05:19:42 +0900

    solve 03

---
+++ Chap01/03.rb
@@ -0,0 +1,7 @@
+str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+p str.split(/\W+/).map(&:length)
+
+###--- memo ---###
+# split : 引数に正規表現を指定した場合は、そのパターンにマッチした部分で文字列を分割する
+#
+# /\W+/ : = [^a-zA-Z0-9_] 非単語構成文字
