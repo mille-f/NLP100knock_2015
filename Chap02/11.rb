1ed0a0b137d117e405dc30c7a6470c8518d1e983
mille-f <f11235tetra@gmail.com>
2015-08-23 04:36:29 +0900

    add unix command

--- Chap02/11.rb
+++ Chap02/11.rb
@@ -12,3 +12,7 @@ end
 ###--- memo ---###
 # gsub : 文字列で置換（正規表現で指定）
 # tr   : 文字列で置換（文字列で指定）
+#
+# unixコマンドその1 sed -e "s/\t/ /g" hightemp.txt
+# unixコマンドその2 cat hightemp.txt | tr "\t" " "
+# unixコマンドその3 expand -t 1 hightemp.txt
