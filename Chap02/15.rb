17907425b64e4e2e391fda9a6bca4da59f4ba1ee
mille-f <f11235tetra@gmail.com>
2015-08-23 09:21:19 +0900

    solve 015

---
+++ Chap02/15.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+fn = ARGV[0]
+n  = ARGV[1].to_i
+
+File.open(fn, "r") do |file|
+  file.reverse_each.lazy.first(n).reverse.each do |line|
+    puts line
+  end
+end
+
+###--- memo ---###
+# reverse_each : 末尾から順に要素の数だけブロックを繰り返す
+# lazy : 遅延評価
+# first : Enumerable オブジェクトの最初のn要素を返す
+#
+# unixコマンド tail -10 ファイル名
